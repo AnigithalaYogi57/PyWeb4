7fa84166f439501041661d3c9efcc4ce1c70779f
Anigithala Yogi <Anigithala@simplifyvms.com>
2023-01-10 22:41:09 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,86 @@
+'''
+print("Hello world")
+a=100
+print(a)
+import keyword
+
+print(keyword.kwlist)
+
+a=100
+print(a)        
+
+'''
+
+
+'''
+print("-------String Handling------")
+x="Python web"
+print(x)
+print(type(x))
+print(id(x))
+
+print(x[0])
+print(x[2])
+print(x[1:5])
+print(x[0:4])
+
+print(x[-1])
+print(x[-3:-1])
+print(x[-1:-3])
+print(x[:4])
+print(x[1:])
+
+print("----")
+print(x[-1:-3])
+print(x[-3:])
+print("-----")
+print("no. of chars:",len(x),"thanks")
+a='1000'
+print(a)
+print(type(a))
+print(len(a))
+
+p=300
+q=500
+print("sum=",p+q)
+'''
+
+
+print("-------Reading data from keyboard------")
+'''
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print(type(a))
+print(type(b))
+c=a+b
+print("sum=",c)
+
+x=input("enter first name:")
+y=input("enter last name:")
+
+print(x+" "+y)
+'''
+a='yogi'
+b='tech'
+c=1000
+d=2000
+e='1234'
+
+print(a+b) #yogitech
+print(c+d) #3000
+print(a+e) #yogi1234
+print(a+str(c)) #yogi1000
+print(c+int(e)) #2234
+print(str(c)+e) #10001234
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,5 @@
+a='pythonweb'
+print(a)
+print(type(a))
+print(id(a))
+print("string length is:", len(a))
