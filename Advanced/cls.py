de5d7c68b8f31ba784afcb4db519e063ca6fa290
Anigithala Yogi <Anigithala@simplifyvms.com>
2023-02-09 22:04:02 +0530

    static or class variables

--- Advanced/cls.py
+++ Advanced/cls.py
@@ -1,3 +1,4 @@
+'''
 print("begin")
 
 class Demo:
@@ -10,6 +11,7 @@ class Demo:
 d=Demo() #d is holding an original(direct) address so, d is pointer variable
 self = d #self is holding same address(indirect address) so self is a referance variable
 x=self # x is also a referance variable
+print(d)
 print(d.a)
 d.firstmethod()
 d.secondmethod()
@@ -17,3 +19,42 @@ print("end")
 
 l=[1,2,3]
 l.append(4)
+'''
+print("-----------static variables------------")
+'''
+class Student:
+    school_name = "lchs"
+    def m1(self):
+        print("inside class",Student.school_name)
+        print("i am in m1 of X")
+        print(self.school_name)
+
+s1=Student()
+print("outside class",Student.school_name)
+print(s1.school_name)
+s1.m1()
+s2=Student()
+print(s2.school_name)
+s3=Student()
+print(s3.school_name)
+
+'''
+class X:
+    a=1000
+    b=2000
+
+    def display(self):
+        print(X.a)
+        print(X.b)
+
+    def modify(self):
+        X.a=3000
+        X.b=4000
+
+x1=X()
+x1.display()
+x1.modify()
+x1.display()
+x2=X()
+x2.display()
+
